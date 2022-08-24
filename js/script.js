36315498de6754bc7f0ddf7451d23722ea4acf03
Ihor <moskovenkoalona@gmail.com>
2022-08-24 14:33:21 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+
+answers[0] = prompt('What is your name', '');
+answers[1] = prompt('What is your surname', '');
+answers[2] = prompt('What is your age', '');
+
+console.log(answers);
